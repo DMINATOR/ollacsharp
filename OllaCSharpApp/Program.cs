a198c0cf7a1547dc4deded74554f0b78931ea94f
Dmitri Kuznetsov <kna_int@mail.ru>
2024-05-08 20:50:10 +0300

    Add initial project structure

---
+++ OllaCSharpApp/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+var ollamaPath = @"E:\dev\ollama\ollama.exe";
+
+try
+{
+    using (Process myProcess = new Process())
+    {
+        myProcess.StartInfo.UseShellExecute = false;
+
+        // You can start any process, HelloWorld is a do-nothing example.
+        myProcess.StartInfo.FileName = ollamaPath;
+        myProcess.StartInfo.CreateNoWindow = true;
+        myProcess.StartInfo.RedirectStandardOutput = true;  
+        
+        myProcess.Start();
+
+         // Synchronously read the standard output of the spawned process.
+        string output = myProcess.StandardOutput.ReadToEnd();  
+        Console.Write(output);
+        myProcess.WaitForExit();
+
+
+        Console.ReadLine();
+
+        Console.WriteLine("Finished");
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
