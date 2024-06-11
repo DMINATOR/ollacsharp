4c179220006221d4260b2451aaf8f8dea05394a0
Dmitri Kuznetsov <kna_int@mail.ru>
2024-06-11 22:52:47 +0300

    Added export results

--- GodotProject/ButtonPress.cs
+++ GodotProject/ButtonPress.cs
@@ -2,6 +2,7 @@ using Godot;
 using GodotSample;
 using System;
 using System.Threading.Tasks;
+using static LLama.LLamaTemplate;
 
 /*
  * Details:
@@ -13,10 +14,15 @@ using System.Threading.Tasks;
 public partial class ButtonPress : Button
 {
     private Executor _executor;
-    
+    private TextEdit _textMessage;
+    private TextEdit _textHistory;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
 	{
+        _textMessage = GetNode<TextEdit>("%TextMessage");
+        _textHistory = GetNode<TextEdit>("%TextHistory");
+
         GD.Print("Loading");
 
         _executor = new Executor();
@@ -35,8 +41,16 @@ public partial class ButtonPress : Button
     {
         GD.Print("Hello world!");
 
-        var response = _executor.SendMessage("Hello how are you ?").Result;
+        ProcessMessage(_textMessage.Text);
+        _textMessage.Text = ""; // clear
+    }
 
-        GD.Print($"Response = {response}");
+    private void ProcessMessage(string message)
+    {
+        _textHistory.Text += $":> '{message}'\r\n";
+
+        var response = _executor.SendMessage(message).Result;
+
+        _textHistory.Text += $":< '{response}'\r\n";
     }
 }
